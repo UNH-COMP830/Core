6f3edeefc4f6ad472757132f7b22dff607c9f21a
asukhu <asukhu@gmail.com>
2016-11-05 02:41:20 -0400

    made some simple modifications

--- code/GameSlam/GameSlam.Web/Controllers/HomeController.cs
+++ code/GameSlam/GameSlam.Web/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace GameSlam.Web.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        {            
             return View();
         }
 
